1aca2520e06964329cd2aae26af62a9c8c5e4322
mirror <mirrory@ciandt.com>
2019-09-20 10:47:01 +0800

    [2019.09.20] New Project Enviroment Building.

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// unregister()用于取消对serviceWorker的注册并返回一个Promise
+// 没有找到注册时,这个Promise返回false,否则不论取消成功与否都返回true
+// 如果想让应用离线工作并能更快加载,可以将unregister()替换成register()
 serviceWorker.unregister();
